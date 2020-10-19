b1f601b391191c43ccc2667aec4eb87d6b3b9391
Rahul kumar <kumarrahulchoudhary91280@gmail.com>
2020-10-19 12:38:38 +0530

    Added Recursion  algo

---
+++ C++/Recursion/print subset.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void solve(string ip,string op)
+{
+    if(ip.length()==0)
+       {
+
+        cout<<op<<" ";
+        return ;}
+        string op1=op;
+        string op2=op+ip[0];
+        ip.erase(ip.begin()+0);
+        solve(ip,op1);
+        solve(ip,op2);
+
+}
+
+int main()
+{
+    string ip;
+    cin>>ip;
+    string op="";
+    solve(ip,op);
+}
