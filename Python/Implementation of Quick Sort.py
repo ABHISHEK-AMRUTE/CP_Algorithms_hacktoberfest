f9bcb076c883593400d67517c54e042ad6c207b3
yashsanghvi2001 <73680203+yashsanghvi2001@users.noreply.github.com>
2020-10-30 18:11:49 +0530

    Create Implementation of Quick Sort.py

---
+++ Python/Implementation of Quick Sort.py
@@ -0,0 +1,47 @@
+def quick_sort(arr):
+    
+    quick_sort_help(arr,0,len(arr)-1)
+
+def quick_sort_help(arr,first,last):
+    
+    if first<last:
+        
+
+        splitpoint = partition(arr,first,last)
+
+        quick_sort_help(arr,first,splitpoint-1)
+        quick_sort_help(arr,splitpoint+1,last)
+
+
+def partition(arr,first,last):
+    
+    pivotvalue = arr[first]
+
+    leftmark = first+1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and arr[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while arr[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark -1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = arr[leftmark]
+            arr[leftmark] = arr[rightmark]
+            arr[rightmark] = temp
+
+    temp = arr[first]
+    arr[first] = arr[rightmark]
+    arr[rightmark] = temp
+
+
+    return rightmark
+arr = [2,5,4,6,7,3,1,4,12,11]
+quick_sort(arr)
+arr
